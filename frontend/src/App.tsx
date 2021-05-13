1202d1dd275e69655eb8b00fdbaaec262646a2a8
Charles Ai <charles.ai@yahoo.com>
2021-05-12 21:50:57 -0400

    Added bootstrap solar template

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bootswatch/dist/solar/bootstrap.min.css'
+import logo from './assets/img/spacex-logo.png'
+// /import logo from './logo.svg';
+//import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="container">
+          <img src={logo} alt="SpaceX Logo" className="img-fluid" />
+          <h1 className="text-center text-light">List of Launches</h1>
+
+        </div>
+        
       </header>
     </div>
   );
